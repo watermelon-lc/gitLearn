d9d12140a41feca9748c78404f51a9880ddebf57
Watermelon <1097457332@qq.com>
2019-10-19 20:13:41 +0800

    bug修复

---
+++ main.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+	if(a != undefined && b != undefined) {
+		return a + b;
+	} else {
+		return 0;
+	}
+	
+}
+let obj = {
+	0: "hello",
+	1: "world",
+	2: "西瓜"
+}
+console.log(add(1));
